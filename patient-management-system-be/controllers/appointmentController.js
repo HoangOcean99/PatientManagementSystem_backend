f6b64cfc9cd96922dc600ed8bca4bb4b20f2c48d
MnhChag <nguyenminhtrang2905@gmail.com>
2026-02-23 18:07:11 +0700

    Task 10.3 - TrangNM - hoàn thành chức năng update/reschedule appointment và phân quyền

--- patient-management-system-be/controllers/appointmentController.js
+++ patient-management-system-be/controllers/appointmentController.js
@@ -11,12 +11,19 @@ export const createAppointment = asyncHandler(async (req, res) => {
   const { patient_id, doctor_id, service_id, appointment_date, start_time, end_time, role } = req.body;
 
   const newAppointment = await appointmentService.createAppointment(patient_id, doctor_id, service_id, appointment_date, start_time, end_time, role);
-  console.log('NEW', newAppointment);
    return res.status(201).json({
       message: "Lịch hẹn đã được tạo thành công",
       data: {
           appointment_id: newAppointment.appointment_id,
           status: newAppointment.status
         }
+    });
+})
+
+export const updateOrReschedule = asyncHandler(async (req, res) => {
+  const { appointment_id, updates, currentUser} = req.body;
+  await appointmentService.updateOrReschedule( appointment_id, updates, currentUser);
+      res.status(200).json({
+      message: "Cập nhật lịch hẹn thành công"
     });
 })
