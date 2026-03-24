a4a67c07526581e2dbf68d8fb8026fee18f56c0d
HoangOcean99 <duonghaiduong090905@gmail.com>
2026-03-25 00:06:00 +0700

    Update baseService.js

--- patient-management-system-be/services/baseService.js
+++ patient-management-system-be/services/baseService.js
@@ -2,7 +2,7 @@ import { supabase } from "../supabaseClient.js"
 import { AppError } from "../utils/app-error.js";
 
 export const testBase = async () => {
-    const {data, error} = await supabase.from('Users').select('*');
-    if(error) throw new AppError(error.message, 500);
+    const { data, error } = await supabase.from('Departments').select('*');
+    if (error) throw new AppError(error.message, 500);
     return data;
 }
