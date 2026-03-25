fb510cc261374ed9d893d2e286091a5e567f2c65
HoangOcean99 <duonghaiduong090905@gmail.com>
2026-03-25 10:56:57 +0700

    Not use stmp, change to https

--- patient-management-system-be/utils/mailer.js
+++ patient-management-system-be/utils/mailer.js
@@ -1,52 +1,44 @@
-import nodemailer from "nodemailer";
+import sgMail from "@sendgrid/mail";
 
-const transporter = nodemailer.createTransport({
-    host: "smtp.sendgrid.net",
-    port: 587,
-    secure: false,
-    auth: {
-        user: "apikey",
-        pass: process.env.SENDGRID_API_KEY,
-    },
-    tls: {
-        rejectUnauthorized: false,
-    },
-});
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 export const sendMail = async (to, subject, html) => {
     try {
-        await transporter.sendMail({
-            from: process.env.EMAIL_USER,
+        await sgMail.send({
             to,
+            from: process.env.EMAIL_USER,
             subject,
             html,
         });
     } catch (err) {
-        console.error("SendMail Error:", err);
+        console.error("SendMail Error:", err.response?.body || err.message);
         throw err;
     }
 };
 
 export const sendMailWithIcal = async (to, subject, html, icalContent = null) => {
     try {
-        const mailOptions = {
-            from: process.env.EMAIL_USER,
+        const msg = {
             to,
+            from: process.env.EMAIL_USER,
             subject,
             html,
         };
 
         if (icalContent) {
-            mailOptions.icalEvent = {
-                filename: "invite.ics",
-                method: "request",
-                content: icalContent,
-            };
+            msg.attachments = [
+                {
+                    content: Buffer.from(icalContent).toString("base64"),
+                    filename: "invite.ics",
+                    type: "text/calendar",
+                    disposition: "attachment",
+                },
+            ];
         }
 
-        await transporter.sendMail(mailOptions);
+        await sgMail.send(msg);
     } catch (err) {
-        console.error("SendMailWithIcal Error:", err);
+        console.error("SendMailWithIcal Error:", err.response?.body || err.message);
         throw err;
     }
 };
